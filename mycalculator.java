eb8a5cce3e214d7e30b2021e66f68e21cd9cf8e4
Артём <97799751+Apkarr@users.noreply.github.com>
2024-07-11 16:40:19 +0300

    Add files via upload

--- mycalculator.java
+++ mycalculator.java
@@ -4,18 +4,11 @@ package Modul1.Classes;
 public class mycalculator {
     public static void main(String[] args) {
         Calculator mycalculator = new Calculator();
-        double sum;
-        double r;
-        double m;
-        double d;
-        mycalculator.a = 3;
-        mycalculator.b = 5;
-        sum = mycalculator.a + mycalculator.b;
-        r = mycalculator.a - mycalculator.b;
-        m = mycalculator.a * mycalculator.b;
-        d = mycalculator.a / mycalculator.b;
-        System.out.println(sum);
-        System.out.println(r);
+        mycalculator.a = 60;
+        mycalculator.b = 3.5;
+        System.out.println(mycalculator);
+        mycalculator.summa();
+        mycalculator.subtraction();
         mycalculator.multiply();
         mycalculator.division();
     }
