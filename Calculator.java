e85a9730f3803eeeeb997ead04422fb22e600c85
Артём <97799751+Apkarr@users.noreply.github.com>
2024-07-15 12:02:29 +0300

    Add files via upload

--- Calculator.java
+++ Calculator.java
@@ -3,18 +3,24 @@ package Modul1.Classes;
 public class Calculator {
     double a;
     double b;
-   public double summa() {
+    public double summa(double a, double b) {
        return a + b;
    }
-   public double subtraction() {
+   public double subtraction(double a, double b) {
        return a - b;
    }
-   public void multiply() {
-       System.out.println("произведение= ");
+   public void multiply(double a, double b) {
+        double mult = a * b;
+       System.out.println("произведение= " + mult);
    }
-   public void division() {
-       System.out.println("частное= ");
+   public void division(double a, double b) {
+        double div = a / b;
+       System.out.println("частное= " + div);
    }
+   double division_a(int c, double d) {
+       return c / d;
+   }
+
     @Override
     public String toString() {
         return "Calculator{" +
